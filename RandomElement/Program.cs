2258adbf397c5e0081857d6e30664399749ea76c
Martin Sillaste <martin.sillaste@tktk.ee>
2024-10-20 12:53:53 +0300

    add week6 code

---
+++ RandomElement/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+string[] snacks = { "sushi", "pizza", "burger", "pasta", "lasagne" };
+
+Random rnd = new Random();
+
+int randomIndex = rnd.Next(0, snacks.Length);
+
+Console.WriteLine($"Tonight we are going to eat {snacks[randomIndex]}.");
---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,28 @@
+﻿string hello = "  hello, world?";
+
+int stringLength = hello.Length;
+
+//string trimmedString = hello.Trim();
+
+hello = hello.Trim();
+
+char firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?', '!');
+
+firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+
+Console.WriteLine(hello);
+
+//int wordCounter = 0;
+//for (int i = 0; i < hello.Length; i++)
+//{
+//    if(char.IsWhiteSpace(hello[i]))
+//    {
+//        wordCounter++;
+//    }
+//}
+
+//Console.WriteLine($"Word count in {hello}; {wordCounter} words.");
